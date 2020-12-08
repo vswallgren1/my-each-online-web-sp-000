acab22e340b14f9791a918e6e4dac5b49eb5ae8f
Vince W <vincewallgren@me.com>
2020-12-08 04:51:32 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,13 @@
-collection = [1, 2, 3, 4]
-def my_each(collection)
-  # code here
-  i = 0
-    while i < collection
-      puts i
-    end
+def my_each(array)
+  # code here	  i = 0
+end 	  while i< array.length
+    yield array[i]
+    i+=1
   end
+
+  return array
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
 end
